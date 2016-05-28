1382aacebbd6e82f68e1e2c8497afc2752378a15
silher13 <silviahervas95@gmail.com>
2016-05-29 01:57:37 +0200

    Lista de preguntas

--- models/index.js
+++ models/index.js
@@ -6,6 +6,8 @@ var Sequelize = require('sequelize');
 // Usar BBDD SQLite:
 //    DATABASE_URL = sqlite:///
 //    DATABASE_STORAGE = quiz.sqlite
+// Usar BBDD Postgres:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
 
 var url, storage;
 
@@ -18,9 +20,9 @@ if (!process.env.DATABASE_URL) {
 }
 
 var sequelize = new Sequelize(url, 
-	 						  { storage: storage,
-				              	omitNull: true 
-				              });
+                              { storage: storage,
+                                omitNull: true 
+                              });
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
@@ -33,9 +35,9 @@ sequelize.sync()
         return Quiz.count()
                 .then(function (c) {
                     if (c === 0) {   // la tabla se inicializa solo si está vacía
-                        return Quiz.create({ question: 'Capital de Italia',
-          	                                 answer: 'Roma'
-          	                               })
+                        return Quiz.bulkCreate([ {question: 'Capital de Italia',   answer: 'Roma'},
+                                                 {question: 'Capital de Portugal', answer: 'Lisboa'}
+                                              ])
                                    .then(function() {
                                         console.log('Base de datos inicializada con datos');
                                     });
