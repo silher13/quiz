cf9ac9235caa93babdba1db76feee2a990ff6d57
silher13 <silviahervas95@gmail.com>
2016-06-01 17:13:51 +0200

    Migraciones y seeders

--- models/index.js
+++ models/index.js
@@ -27,27 +27,6 @@ var sequelize = new Sequelize(url,
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
-
-// sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync()
-    .then(function() {
-        // Ya se han creado las tablas necesarias.
-        return Quiz.count()
-                .then(function (c) {
-                    if (c === 0) {   // la tabla se inicializa solo si está vacía
-                        return Quiz.bulkCreate([ {question: 'Capital de Italia',   answer: 'Roma'},
-                                                 {question: 'Capital de Portugal', answer: 'Lisboa'}
-                                              ])
-                                   .then(function() {
-                                        console.log('Base de datos inicializada con datos');
-                                    });
-                    }
-                });
-    })
-    .catch(function(error) {
-        console.log("Error Sincronizando las tablas de la BBDD:", error);
-        process.exit(1);
-    });
-
+//La sincronizacion e inicializacion de la BBDD se realiza a partir de ahora con los comandos de sequelize-cli y no en index.js
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
